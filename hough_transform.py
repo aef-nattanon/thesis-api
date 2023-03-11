cf257b415646686cac0f3d9630c4b52c7fd74c3b
aefnattanon <aef.nattanon@gmail.com>
2023-03-11 22:40:19 +0700

    Fix hough

--- hough_transform.py
+++ hough_transform.py
@@ -2,67 +2,82 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skimage.color import rgb2gray
 from skimage.transform import rotate
-from skimage.transform import (hough_line, hough_line_peaks)
+from skimage.transform import (hough_line, hough_line_peaks, resize)
 from scipy.stats import mode
 from skimage import io
 import cv2 as cv
 from skimage.filters import threshold_otsu, sobel
 
+
 def binarizeImage(RGB_image):
 
-  image = rgb2gray(RGB_image)
-  threshold = threshold_otsu(image)
-  bina_image = image < threshold
-  
-  return bina_image
+    image = rgb2gray(RGB_image)
+    threshold = threshold_otsu(image)
+    bina_image = image < threshold
+
+    return bina_image
+
 
 def findEdges(bina_image):
-  
-  image_edges = sobel(bina_image)
 
-  # plt.imshow(bina_image, cmap='gray')
-  # plt.axis('off')
-  # plt.title('Binary Image Edges')
+    image_edges = sobel(bina_image)
 
-  return image_edges
+    # plt.imshow(bina_image, cmap='gray')
+    # plt.axis('off')
+    # plt.title('Binary Image Edges')
+
+    return image_edges
+
 
 def findTiltAngle(image_edges):
-  
-  h, theta, d = hough_line(image_edges)
-  accum, angles, dists = hough_line_peaks(h, theta, d)
-  angle = np.rad2deg(mode(angles, keepdims=True)[0][0])
-  
-  if (angle < 0):
-    return angle + 90
-  else:
-    return angle - 90
+
+    h, theta, d = hough_line(image_edges)
+    accum, angles, dists = hough_line_peaks(h, theta, d)
+    angle = np.rad2deg(mode(angles, keepdims=True)[0][0])
+
+    if (angle < 0):
+        return angle + 90
+    else:
+        return angle - 90
+
 
 def rotateImage(RGB_image, angle):
+    height, width, depth = RGB_image.shape
+    max_size = width
+    if height > width:
+        add_more = int((height - width)/2)
+        resized_img = cv.copyMakeBorder(RGB_image, 0, 0, add_more, add_more,
+                                        cv.BORDER_CONSTANT, None, value=0)
+    else:
+        add_more = int((width - height)/2)
+        resized_img = cv.copyMakeBorder(RGB_image, add_more, add_more, 0, 0,
+                                        cv.BORDER_CONSTANT, None, value=0)
 
-  fixed_image = rotate(RGB_image, angle)
+    fixed_image = rotate(resized_img, angle)
 
-  return fixed_image
+    return fixed_image
+
 
 def generalPipeline(img, is_save=False):
-  image = io.imread(img)
-  try:
-    bina_image = binarizeImage(image)
-    image_edges = findEdges(bina_image)
-    angle = findTiltAngle(image_edges)
-    fixed_image = rotateImage(image, angle)
-    # =============================================================================
-    if is_save:
-      io.imsave(img, (fixed_image*255).astype(np.uint8))
-    else:
-      plt.imshow(fixed_image)
-      plt.axis('off')
-      plt.title('Fixed Image')
-      plt.show()
-    # =============================================================================
-    return fixed_image, img
-  except:
-    return image, img
-
+    image = io.imread(img)
+    try:
+        bina_image = binarizeImage(image)
+        image_edges = findEdges(bina_image)
+        angle = findTiltAngle(image_edges)
+        fixed_image = rotateImage(image, angle)
+        # =============================================================================
+        if is_save:
+            io.imsave(img, (fixed_image*255).astype(np.uint8))
+        else:
+            plt.imshow(fixed_image)
+            plt.axis('off')
+            plt.title('Fixed Image')
+            plt.show()
+        # =============================================================================
+        return fixed_image, img
+    except:
+        print('error')
+        return image, img
 
 
 if __name__ == "__main__":
@@ -72,5 +87,5 @@ if __name__ == "__main__":
     generalPipeline(img_ptah_4, False)
     img_ptah_5 = './images/meter_for_test_5.jpeg'
     generalPipeline(img_ptah_5, False)
-    img_ptah_5 = './run_meter/1677607918/crops/model 1/img1.jpg'
-    generalPipeline(img_ptah_5, False)
+    # img_ptah_5 = './run_meter/1677607918/crops/model 1/img1.jpg'
+    # generalPipeline(img_ptah_5, False)
--- prediction.py
+++ prediction.py
@@ -11,6 +11,7 @@ from hough_transform import *
 
 from datetime import datetime
 
+
 def super_resolution(img, lapsrn_model):
     frame = cv.imread(img)
     return super_resolution_by_image(img, frame, lapsrn_model)
@@ -25,12 +26,14 @@ def super_resolution_by_image(img, frame, lapsrn_model):
         cv.imwrite(img, frame)
     return img
 
+
 def my_bitwise_not_by_image(frame, data_class, data_name):
     frame_thresh = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     if data_name == MODEL_2_NAME:
         return cv.bitwise_not(frame_thresh)
     else:
         return frame_thresh
+
 
 def my_bitwise_not(img, data_class, data_name):
     frame = cv.imread(img)
@@ -39,6 +42,7 @@ def my_bitwise_not(img, data_class, data_name):
         return cv.bitwise_not(frame_thresh)
     else:
         return frame_thresh
+
 
 def my_check(img):
     frame = cv.imread(img)
@@ -73,10 +77,10 @@ def meter_detection(img, dri, meter_model):
     # print('🐞 ---results.pandas().xyxy[0]---', results.pandas().xyxy[0])
     results.crop(save_dir=save_dir, save=True)
     # results.save(save_dir=save_dir+'/result')
-    # print('🐞 ---results---', results) 
+    # print('🐞 ---results---', results)
 
     data_0 = results.pandas().xyxy[0]
-    # print('🐞 ---data_0---', data_0) 
+    # print('🐞 ---data_0---', data_0)
     data_class = data_0.iloc[0]['class']
     data_name = data_0.iloc[0]['name']
     return save_dir+'/crops/'+data_0.iloc[0]['name']+'/'+Path(img).stem+'.jpg', data_class, data_name
@@ -88,15 +92,17 @@ def my_detection(img, meter_model, number_model, lapsrn_model):
     curr_dt = datetime.now()
     ts = str(int(round(curr_dt.timestamp())))
     # ts = '1'
-    meter_dir, data_class, data_name  = meter_detection(img, ts, meter_model)
+    meter_dir, data_class, data_name = meter_detection(img, ts, meter_model)
     fixed_image, meter_dir = generalPipeline(meter_dir, True)
     fixed_image = my_bitwise_not(meter_dir, data_class, data_name)
-    
+
     # fixed_image = my_bitwise_not_by_image(fixed_image, data_class, data_name)
-    meter_my_check_dir = super_resolution_by_image(meter_dir, fixed_image, lapsrn_model)
+    meter_my_check_dir = super_resolution_by_image(
+        meter_dir, fixed_image, lapsrn_model)
 
     file_name = ntpath.basename(meter_my_check_dir)
-    r_json, data, number_path = number_detection(meter_my_check_dir, ts, number_model, file_name)
+    r_json, data, number_path = number_detection(
+        meter_my_check_dir, ts, number_model, file_name)
     # print("📜 results json: ", r_json)
     print("✅❌ result data: ", data)
     return json.loads(r_json), data, ts, meter_my_check_dir, number_path
@@ -109,7 +115,7 @@ def initialize_models():
     # meter_model
     meter_model = torch.hub.load(
         'ultralytics/yolov5', 'custom', path='./models/best-models-meter.pt')
-        # 'ultralytics/yolov5', 'custom', path='./models/best-meter.pt')
+    # 'ultralytics/yolov5', 'custom', path='./models/best-meter.pt')
     # lapsrn_model
     lapsrn_model = cv.dnn_superres.DnnSuperResImpl_create()
     lapsrn_model.readModel("./models/LapSRN_x4.pb")
@@ -124,8 +130,8 @@ def initialize_models():
 if __name__ == "__main__":
     print('ini grayscale-to-negative-2 🧑🏻‍💻')
     number_model, meter_model, lapsrn_model = initialize_models()
-    # my_detection('./images/img1.jpeg', meter_model, number_model, lapsrn_model)
-    # my_detection('./images/img2.jpeg', meter_model, number_model, lapsrn_model)
+    my_detection('./images/img1.jpeg', meter_model, number_model, lapsrn_model)
+    my_detection('./images/img3.jpeg', meter_model, number_model, lapsrn_model)
     # my_detection('https://aef-nattanon.github.io/demo3.jpg', meter_model, number_model, lapsrn_model)
-    my_detection('https://aef-nattanon.github.io/img2.jpeg', meter_model, number_model, lapsrn_model)
+    # my_detection('https://aef-nattanon.github.io/img2.jpeg', meter_model, number_model, lapsrn_model)
     print('end 🤖')
